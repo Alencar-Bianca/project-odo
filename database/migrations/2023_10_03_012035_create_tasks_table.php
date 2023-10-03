634bdc83b8ffb9e8ab3534b8af7b8e9dcb84bdc9
alencar-bianca <alencar-bianca@hotmail.com>
2023-10-02 22:32:07 -0300

    create migrations

--- database/migrations/2023_10_03_012035_create_tasks_table.php
+++ database/migrations/2023_10_03_012035_create_tasks_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Models\User;
+use App\Models\Category;
 return new class extends Migration
 {
     /**
@@ -11,11 +12,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable(false);
-            $table->string('color')->default('#FFFFFF');
+            $table->string('description');
+            $table->dateTime('due_date');
             $table->foreignIdFor(User::class)->references('id')->on('users');
+            $table->foreignIdFor(Category::class)->references('id')->on('categories');
             $table->timestamps();
         });
     }
@@ -25,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('tasks');
     }
 };
